ef2773e2ee22e1bc0cf1ff06a7e960a92839676d
Hasna Septi Dewi <91710674+hasnaasd05@users.noreply.github.com>
2024-03-19 09:44:52 +0700

    Update dashboard.blade.php

--- resources/views/dosen/dashboard.blade.php
+++ resources/views/dosen/dashboard.blade.php
@@ -7,6 +7,15 @@
     <div class="row">
         <div class="col-12">
             <div class="col-xl-12 col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <center>
+                            <h3>Selamat Datang di
+                                Sistem Kurikulum Itenas !</h3>
+                        </center>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
